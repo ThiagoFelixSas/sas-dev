7945149504a8973e042d2e84c3fd416a77c199b4
Thiago Félix <Thiago.Felix@sas.com>
2026-03-16 22:27:20 -0300

    Criando_arquivo_gitIgnore

--- env.py
+++
@@ -1,4 +0,0 @@
-# Lembre-se: este token expira. Se der erro 401, gere um novo no link do navegador.
-# .env - Arquivo de configuração (NUNCA enviar ao Git)
-SAS_TOKEN="seu_token_aqui"
-SAS_HOST="create.demo.sas.com"
