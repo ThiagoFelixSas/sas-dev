2f63523fc6ec66fa1877b2ac8e98b19f7ebb75f2
Thiago Félix <Thiago.Felix@sas.com>
2026-03-16 22:09:33 -0300

    Conexão_com_sas

--- projeto_piloto_gecen/dags/agendamento_gecen.py
+++ projeto_piloto_gecen/dags/agendamento_gecen.py
@@ -1,3 +1,10 @@
 from airflow import DAG  # Linha 1: Importa a "ferramenta de agendamento"
 from datetime import datetime  # Linha 2: Importa o "calendário" do Python
 from airflow.operators.python import PythonOperator # Linha 3: Ferramenta para rodar seu código Python
+
+# Linha 4: Definindo o nome do agendamento e o cronograma
+dag_gecen = DAG(
+    'agendamento_conexao_sas',       # O nome que vai aparecer no painel do Airflow
+    start_date=datetime(2026, 2, 25), # Data de início (hoje)
+    schedule_interval='@daily'       # Frequência: Roda uma vez por dia (ex: meia-noite)
+)
