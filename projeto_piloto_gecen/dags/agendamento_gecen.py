90c7502fed0ceff991d2dd4338ca514b2babc328
Thiago Félix <Thiago.Felix@sas.com>
2026-03-16 22:10:44 -0300

    Conexão_cas

--- projeto_piloto_gecen/dags/agendamento_gecen.py
+++ projeto_piloto_gecen/dags/agendamento_gecen.py
@@ -6,5 +6,17 @@ from airflow.operators.python import PythonOperator # Linha 3: Ferramenta para r
 dag_gecen = DAG(
     'agendamento_conexao_sas',       # O nome que vai aparecer no painel do Airflow
     start_date=datetime(2026, 2, 25), # Data de início (hoje)
-    schedule_interval='@daily'       # Frequência: Roda uma vez por dia (ex: meia-noite)
+    schedule='@daily'       # Frequência: Roda uma vez por dia (ex: meia-noite)
+)
+
+# Definindo a tarefa que o Airflow vai executar
+def executar_script_sas():
+    # Aqui o Airflow chama a lógica que você já validou no outro arquivo
+    print("Iniciando o Pipeline de Automação do SAS...")
+
+# Criando o operador que liga tudo
+tarefa_final = PythonOperator(
+    task_id='executar_conexao_sas',
+    python_callable=executar_script_sas, # Chama a função acima
+    dag=dag_gecen
 )
